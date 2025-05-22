49d805996e10631e37a890a22b4012998055a5c5
Jackbringas <102544709+Jackbringas@users.noreply.github.com>
2025-05-22 16:32:15 -0500

    Initialize login-fetch

---
+++ src/api/models.py
@@ -0,0 +1,27 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import String, Boolean
+from sqlalchemy.orm import Mapped, mapped_column
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str]= mapped_column(String(120), nullable=False)
+    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    phone: Mapped[str] = mapped_column(String(20))
+
+
+
+
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "email": self.email,
+            "phone": self.phone,
+            # do not serialize the password, its a security breach
+        }
+    
