12e8b029e3f7fa81701aabd4b83c09b8d51f39e0
Jackbringas <102544709+Jackbringas@users.noreply.github.com>
2025-05-22 16:32:14 -0500

    Initialize 65-log-in-end-point-with-jwt

---
+++ src/api/models.py
@@ -0,0 +1,24 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import String, Boolean
+from sqlalchemy.orm import Mapped, mapped_column
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str]= mapped_column(String(120), nullable=False)
+    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+
+
+
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "email": self.email,
+            # do not serialize the password, its a security breach
+        }
+    
